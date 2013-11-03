211a915d31398897f1b9b609540aa2c841ec2d77
Sualeh Fatehi <sualeh@hotmail.com>
2013-11-03 13:38:33 -0500

    Initial commit

---
+++ src/main/java/us/fatehi/sualeh/chapter01/Question1_1.java
@@ -0,0 +1,77 @@
+package us.fatehi.sualeh.chapter01;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 1.1 Implement an algorithm to determine if a string has all unique
+ * characters. What if you can not use additional data structures?
+ */
+public class Question1_1 {
+
+	// We can reduce our space usage a little bit by using a bit vector. We will
+	// assume, in the below code, that the string is only lower case ‘a’ through
+	// ‘z’. This will allow us to use just a single int
+	public static boolean isUniqueChars(final String str) {
+
+		// Basic error checking
+		if (str == null || str.isEmpty()) {
+			return true;
+		}
+
+		int checker = 0;
+		for (int i = 0; i < str.length(); ++i) {
+			final int val = str.charAt(i) - 'a';
+			if ((checker & 1 << val) > 0) {
+				return false;
+			}
+			checker |= 1 << val;
+		}
+		return true;
+	}
+
+	// For simplicity, assume char set is ASCII (if not, we need to increase the
+	// storage size. The rest of the logic would be the same). NOTE: This is a
+	// great thing to point out to your interviewer!
+	// Time complexity is O(n), where n is the length of the string, and space
+	// complexity is O(n).
+	public static boolean isUniqueChars2(final String str) {
+
+		// Basic error checking
+		if (str == null || str.isEmpty()) {
+			return true;
+		}
+
+		// Assume only ASCII characters, not Unicode
+		final boolean[] char_set = new boolean[256];
+
+		for (int i = 0; i < str.length(); i++) {
+			final int val = str.charAt(i);
+			if (char_set[val]) {
+				return false;
+			}
+			char_set[val] = true;
+		}
+		return true;
+	}
+
+	public static boolean isUniqueCharsUnicode(final String str) {
+
+		// Basic error checking
+		if (str == null || str.isEmpty()) {
+			return true;
+		}
+
+		final Set<Integer> char_set = new HashSet<>();
+
+		for (int i = 0; i < str.length(); i++) {
+			final int val = Character.codePointAt(str, i);
+			if (char_set.contains(val)) {
+				return false;
+			}
+			char_set.add(val);
+		}
+		return true;
+	}
+
+}
---
+++ src/test/java/us/fatehi/sualeh/chapter01/TestQuestion1_1.java
@@ -0,0 +1,71 @@
+package us.fatehi.sualeh.chapter01;
+
+import static org.junit.Assert.assertEquals;
+import static us.fatehi.sualeh.chapter01.Question1_1.isUniqueChars;
+import static us.fatehi.sualeh.chapter01.Question1_1.isUniqueChars2;
+import static us.fatehi.sualeh.chapter01.Question1_1.isUniqueCharsUnicode;
+
+import org.junit.Test;
+
+public class TestQuestion1_1 {
+
+	@Test
+	public void test1Question1_1() {
+		final String[] words = { null, "", "abcde", "hello", "漢字", "漢漢" };
+		final boolean[] hasUniqueChars = { true, true, true, false, true, false };
+
+		for (int i = 0; i < words.length; i++) {
+			final String word = words[i];
+			final boolean expected = hasUniqueChars[i];
+			assertEquals(String.format("\"%s\" %s unique characters", word,
+					expected ? "has" : "does not have"), expected,
+					isUniqueChars(word));
+		}
+	}
+
+	@Test(expected = ArrayIndexOutOfBoundsException.class)
+	public void test2aQuestion1_1() {
+		final String[] words = { null, "", "abcde", "hello", "漢字", "漢漢" };
+		final boolean[] hasUniqueChars = { true, true, true, false, false,
+				true, true, true };
+
+		for (int i = 0; i < words.length; i++) {
+			final String word = words[i];
+			final boolean expected = hasUniqueChars[i];
+			assertEquals(String.format("\"%s\" %s unique characters", word,
+					expected ? "has" : "does not have"), expected,
+					isUniqueChars2(word));
+		}
+	}
+
+	@Test
+	public void test2Question1_1() {
+		final String[] words = { null, "", "abcde", "hello", "apple", "kite",
+				"padle", };
+		final boolean[] hasUniqueChars = { true, true, true, false, false,
+				true, true, };
+
+		for (int i = 0; i < words.length; i++) {
+			final String word = words[i];
+			final boolean expected = hasUniqueChars[i];
+			assertEquals(String.format("\"%s\" %s unique characters", word,
+					expected ? "has" : "does not have"), expected,
+					isUniqueChars2(word));
+		}
+	}
+
+	@Test
+	public void test3Question1_1() {
+		final String[] words = { null, "", "abcde", "hello", "漢字", "漢漢" };
+		final boolean[] hasUniqueChars = { true, true, true, false, true, false };
+
+		for (int i = 0; i < words.length; i++) {
+			final String word = words[i];
+			final boolean expected = hasUniqueChars[i];
+			assertEquals(String.format("\"%s\" %s unique characters", word,
+					expected ? "has" : "does not have"), expected,
+					isUniqueCharsUnicode(word));
+		}
+	}
+
+}
