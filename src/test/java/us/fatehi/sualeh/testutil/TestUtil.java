bd837c44f03bc8f2fca45ab1578229b9fd145e44
Sualeh Fatehi <sualeh@hotmail.com>
2013-11-22 22:25:53 -0500

    Chapter 1, 7

---
+++ src/test/java/us/fatehi/sualeh/testutil/TestUtil.java
@@ -0,0 +1,28 @@
+package us.fatehi.sualeh.testutil;
+
+import java.util.Scanner;
+
+public class TestUtil
+{
+
+  public static int[][] readIntArray(final String[] inputLines)
+  {
+  
+    final int[][] twoDim = new int[inputLines.length][inputLines.length];
+  
+    for (int row = 0; row < inputLines.length; row++)
+    {
+      final String line = inputLines[row];
+      final Scanner s = new Scanner(line);
+      for (int col = 0; col < inputLines.length; col++)
+      {
+        if (s.hasNextInt())
+        {
+          twoDim[row][col] = s.nextInt();
+        }
+      }
+    }
+    return twoDim;
+  }
+
+}
