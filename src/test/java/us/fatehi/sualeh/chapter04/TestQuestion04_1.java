7d747c8063a3450225149a72a644c690d1f1d167
Sualeh Fatehi <sualeh@hotmail.com>
2014-01-12 20:25:37 -0500

    Better function for depth

--- src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_1.java
+++ src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_1.java
@@ -16,24 +16,24 @@ public class TestQuestion04_1
   {
 
     final TreeNode balancedRoot = new TreeNode();
-    makeBalancedTree(balancedRoot, 3);
+    makeBalancedTree(balancedRoot, 4);
 
     final TreeNode unbalancedRoot = new TreeNode();
-    makeUnbalancedTree(unbalancedRoot, 3);
+    makeUnbalancedTree(unbalancedRoot, 4);
 
     assertTrue(Question04_1.isBalanced(balancedRoot));
     assertEquals(4, Question04_1.maxDepth(balancedRoot));
     assertEquals(4, Question04_1.minDepth(balancedRoot));
-    
+
     assertTrue(!Question04_1.isBalanced(unbalancedRoot));
     assertEquals(4, Question04_1.maxDepth(unbalancedRoot));
     assertEquals(2, Question04_1.minDepth(unbalancedRoot));
 
   }
 
-  private void makeBalancedTree(final TreeNode node, final int max)
+  private void makeBalancedTree(final TreeNode node, final int maxDepth)
   {
-    if (max == 0)
+    if (maxDepth == 1 || node == null)
     {
       return;
     }
@@ -45,13 +45,13 @@ public class TestQuestion04_1
     node.left = nodeL;
     node.right = nodeR;
 
-    makeBalancedTree(nodeL, max - 1);
-    makeBalancedTree(nodeR, max - 1);
+    makeBalancedTree(nodeL, maxDepth - 1);
+    makeBalancedTree(nodeR, maxDepth - 1);
   }
 
-  private void makeUnbalancedTree(final TreeNode node, final int max)
+  private void makeUnbalancedTree(final TreeNode node, final int maxDepth)
   {
-    if (max == 0)
+    if (maxDepth == 1 || node == null)
     {
       return;
     }
@@ -63,7 +63,7 @@ public class TestQuestion04_1
     node.left = nodeL;
     node.right = nodeR;
 
-    makeUnbalancedTree(nodeL, max - 1);
+    makeUnbalancedTree(nodeL, maxDepth - 1);
   }
 
 }
