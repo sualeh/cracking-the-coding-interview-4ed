5889e6324e6917c4cf4e95a53642f9c40a95898b
Sualeh Fatehi <sualeh@hotmail.com>
2014-01-21 21:55:25 -0500

    4.2

---
+++ src/main/java/us/fatehi/sualeh/chapter04/Question04_2.java
@@ -0,0 +1,61 @@
+package us.fatehi.sualeh.chapter04;
+
+
+import java.util.Stack;
+
+import us.fatehi.sualeh.util.Graph;
+import us.fatehi.sualeh.util.Graph.Node;
+import us.fatehi.sualeh.util.Graph.State;
+
+/**
+ * 4.2 Given a directed graph, design an algorithm to find out whether
+ * there is a route between two nodes.
+ */
+public class Question04_2
+{
+
+  /*
+   * This problem can be solved by just simple graph traversal, such as
+   * depth first search or breadth first search. We start with one of
+   * the two nodes and, during traversal, check if the other node is
+   * found. We should mark any node found in the course of the algorithm
+   * as ‘already visited’ to avoid cycles and repetition of the nodes.
+   */
+  
+  public static boolean search(Graph g, Node start, Node end)
+  {
+    Stack<Node> q = new Stack<Node>();
+    for (Node u: g.getNodes())
+    {
+      u.state = State.Unvisited;
+    }
+    start.state = State.Visiting;
+    q.add(start);
+    Node u;
+    while (!q.isEmpty())
+    {
+      u = q.pop();
+      if (u != null)
+      {
+        for (Node v: u.getAdjacent())
+        {
+          if (v.state == State.Unvisited)
+          {
+            if (v == end)
+            {
+              return true;
+            }
+            else
+            {
+              v.state = State.Visiting;
+              q.add(v);
+            }
+          }
+        }
+        u.state = State.Visited;
+      }
+    }
+    return false;
+  }
+
+}
---
+++ src/main/java/us/fatehi/sualeh/util/Graph.java
@@ -0,0 +1,97 @@
+package us.fatehi.sualeh.util;
+
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class Graph
+{
+
+  public class Edge
+  {
+
+    public final Node from;
+    public final Node to;
+
+    private Edge(final Node from, final Node to)
+    {
+      this.from = from;
+      this.to = to;
+    }
+
+  }
+
+  public class Node
+  {
+    public final String name;
+    public final Collection<Edge> inEdges;
+    public final Collection<Edge> outEdges;
+    public State state;
+
+    private Node(final String name)
+    {
+      this.name = name;
+      inEdges = new HashSet<>();
+      outEdges = new HashSet<>();
+    }
+
+    public Node addEdgeTo(final Node node)
+    {
+      final Edge e = new Edge(this, node);
+      outEdges.add(e);
+      node.inEdges.add(e);
+      return this;
+    }
+
+    public Collection<Node> getAdjacent()
+    {
+      final Collection<Node> adjacentNodes = new HashSet<>();
+      for (final Edge edge: outEdges)
+      {
+        adjacentNodes.add(edge.to);
+      }
+      return adjacentNodes;
+    }
+
+    @Override
+    public String toString()
+    {
+      return name;
+    }
+
+  }
+
+  public enum State
+  {
+    Unvisited,
+    Visited,
+    Visiting;
+  }
+
+  private final Map<String, Node> nodes;
+
+  public Graph()
+  {
+    nodes = new HashMap<>();
+  }
+
+  public Node addNode(final String name)
+  {
+    final Node node = new Node(name);
+    nodes.put(name, node);
+    return node;
+  }
+
+  public Node getNode(final String name)
+  {
+    return nodes.get(name);
+  }
+
+  public Collection<Node> getNodes()
+  {
+    return nodes.values();
+  }
+
+}
