cdda9e5080f3e25282e652f9e7ad236f0758a07c
Sualeh Fatehi <sualeh.fatehi@gmail.com>
2014-01-27 20:49:03 -0500

    Removing duplicated code

--- src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_1.java
+++ src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_1.java
@@ -7,29 +7,13 @@ import static us.fatehi.sualeh.chapter04.Question04_1.isBalanced;
 import static us.fatehi.sualeh.chapter04.Question04_1.maxDepth;
 import static us.fatehi.sualeh.chapter04.Question04_1.minDepth;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import us.fatehi.sualeh.util.TreeNode;
-import us.fatehi.sualeh.util.TreeNodePrinter;
+import us.fatehi.sualeh.testutil.TreeNodeTest;
 
 public class TestQuestion04_1
+  extends TreeNodeTest
 {
-
-  private TreeNode balancedRoot;
-  private TreeNode unbalancedRoot;
-
-  @Before
-  public void setup()
-  {
-    balancedRoot = new TreeNode();
-    makeBalancedTree(balancedRoot, 4);
-    TreeNodePrinter.print(balancedRoot);
-
-    unbalancedRoot = new TreeNode();
-    makeUnbalancedTree(unbalancedRoot, 4);
-    TreeNodePrinter.print(unbalancedRoot);
-  }
 
   @Test
   public void test1Question04_1()
@@ -40,44 +24,9 @@ public class TestQuestion04_1
     assertEquals(4, minDepth(balancedRoot));
 
     assertTrue(!isBalanced(unbalancedRoot));
-    assertEquals(4, maxDepth(unbalancedRoot));
+    assertEquals(5, maxDepth(unbalancedRoot));
     assertEquals(2, minDepth(unbalancedRoot));
 
-  }
-
-  private void makeBalancedTree(final TreeNode node, final int maxDepth)
-  {
-    if (maxDepth == 1 || node == null)
-    {
-      return;
-    }
-
-    TreeNode nodeL, nodeR;
-
-    nodeL = new TreeNode();
-    nodeR = new TreeNode();
-    node.addLeft(nodeL);
-    node.addRight(nodeR);
-
-    makeBalancedTree(nodeL, maxDepth - 1);
-    makeBalancedTree(nodeR, maxDepth - 1);
-  }
-
-  private void makeUnbalancedTree(final TreeNode node, final int maxDepth)
-  {
-    if (maxDepth == 1 || node == null)
-    {
-      return;
-    }
-
-    TreeNode nodeL, nodeR;
-
-    nodeL = new TreeNode();
-    nodeR = new TreeNode();
-    node.addLeft(nodeL);
-    node.addRight(nodeR);
-
-    makeUnbalancedTree(nodeL, maxDepth - 1);
   }
 
 }
--- src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_6.java
+++ src/test/java/us/fatehi/sualeh/chapter04/TestQuestion04_6.java
@@ -2,92 +2,89 @@ package us.fatehi.sualeh.chapter04;
 
 
 import static org.junit.Assert.assertEquals;
-import static us.fatehi.sualeh.util.TreeNodePrinter.print;
+
+import java.util.Arrays;
+import java.util.Comparator;
 
 import org.junit.Before;
 import org.junit.Test;
 
+import us.fatehi.sualeh.testutil.TreeNodeTest;
 import us.fatehi.sualeh.util.TreeNode;
 
 public class TestQuestion04_6
+  extends TreeNodeTest
 {
 
-  private TreeNode[] nodes;
+  protected TreeNode[] nodes;
 
+  @Override
   @Before
   public void setup()
   {
-    nodes = makeUnbalancedTree();
-    final TreeNode root = nodes[0];
-    print(root);
+    super.setup();
+    
+    nodes = new TreeNode[unbalancedNodes.length];
+    System.arraycopy(unbalancedNodes, 0, nodes, 0, unbalancedNodes.length);
+    Arrays.sort(nodes, new Comparator<TreeNode>()
+    {
+
+      @Override
+      public int compare(final TreeNode node1, final TreeNode node2)
+      {
+        return node1.value() - node2.value();
+      }
+    });
   }
 
   @Test
   public void test1Question04_6_2()
   {
     // Parent is common ancestor
-    assertEquals(nodes[6],
-                 Question04_6_2.commonAncestor(nodes[0], nodes[8], nodes[5]));
+    assertEquals(nodes[6], Question04_6_2.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[5]));
     // Common ancestor a few levels up
-    assertEquals(nodes[7],
-                 Question04_6_2.commonAncestor(nodes[0], nodes[8], nodes[2]));
+    assertEquals(nodes[7], Question04_6_2.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[2]));
     // Common ancestor is root
-    assertEquals(nodes[0],
-                 Question04_6_2.commonAncestor(nodes[0], nodes[8], nodes[4]));
+    assertEquals(unbalancedRoot, Question04_6_2.commonAncestor(unbalancedRoot,
+                                                               nodes[8],
+                                                               nodes[4]));
     // One node is the parent of another
-    assertEquals(nodes[6],
-                 Question04_6_2.commonAncestor(nodes[0], nodes[8], nodes[6]));
+    assertEquals(nodes[6], Question04_6_2.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[6]));
     // Same nodes
-    assertEquals(nodes[8],
-                 Question04_6_2.commonAncestor(nodes[0], nodes[8], nodes[8]));
+    assertEquals(nodes[8], Question04_6_2.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[8]));
   }
 
   @Test
   public void test1Question04_6_3()
   {
     // Parent is common ancestor
-    assertEquals(nodes[6],
-                 Question04_6_3.commonAncestor(nodes[0], nodes[8], nodes[5]));
+    assertEquals(nodes[6], Question04_6_3.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[5]));
     // Common ancestor a few levels up
-    assertEquals(nodes[7],
-                 Question04_6_3.commonAncestor(nodes[0], nodes[8], nodes[2]));
+    assertEquals(nodes[7], Question04_6_3.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[2]));
     // Common ancestor is root
-    assertEquals(nodes[0],
-                 Question04_6_3.commonAncestor(nodes[0], nodes[8], nodes[4]));
+    assertEquals(unbalancedRoot, Question04_6_3.commonAncestor(unbalancedRoot,
+                                                               nodes[8],
+                                                               nodes[4]));
     // One node is the parent of another
-    assertEquals(nodes[6],
-                 Question04_6_3.commonAncestor(nodes[0], nodes[8], nodes[6]));
+    assertEquals(nodes[6], Question04_6_3.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[6]));
     // Same nodes
-    assertEquals(nodes[8],
-                 Question04_6_3.commonAncestor(nodes[0], nodes[8], nodes[8]));
-  }
-
-  private TreeNode[] makeUnbalancedTree()
-  {
-    final TreeNode[] nodes = {
-        new TreeNode(0),
-        new TreeNode(1),
-        new TreeNode(2),
-        new TreeNode(3),
-        new TreeNode(4),
-        new TreeNode(5),
-        new TreeNode(6),
-        new TreeNode(7),
-        new TreeNode(8),
-        new TreeNode(9)
-    };
-
-    nodes[0].addLeft(nodes[7]);
-    nodes[0].addRight(nodes[3]);
-    nodes[7].addLeft(nodes[2]);
-    nodes[7].addRight(nodes[6]);
-    nodes[6].addLeft(nodes[5]);
-    nodes[6].addRight(nodes[8]);
-    nodes[3].addRight(nodes[9]);
-    nodes[9].addLeft(nodes[4]);
-    nodes[4].addLeft(nodes[1]);
-
-    return nodes;
+    assertEquals(nodes[8], Question04_6_3.commonAncestor(unbalancedRoot,
+                                                         nodes[8],
+                                                         nodes[8]));
   }
 
 }
