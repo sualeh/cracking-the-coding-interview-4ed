581b88460576b5cd017bbcabb45315eb26d2e258
Sualeh Fatehi <sualeh.fatehi@gmail.com>
2014-02-04 21:55:19 -0500

    8.2

--- src/main/java/us/fatehi/sualeh/chapter08/Question08_2.java
+++ src/main/java/us/fatehi/sualeh/chapter08/Question08_2.java
@@ -19,18 +19,17 @@ import us.fatehi.sualeh.util.Point;
 public class Question08_2
 {
   /*
-   * Part 1: (For clarity, we will solve this part assuming an X by Y
-   * grid) Each path has (X-1)+(Y-1) steps. Imagine the following paths:
-   * X X Y Y X (move right -> right -> down -> down -> right) X Y X Y X
-   * (move right -> down -> right -> down -> right) ... Each path can be
-   * fully represented by the moves at which we move right. That is, if
-   * I were to ask you which path you took, you could simply say “I
-   * moved right on step 3 and 4.” Since you must always move right X-1
-   * times, and you have X-1 + Y-1 total steps, you have to pick X-1
-   * times to move right out of X-1+Y-1 choices. Thus, there are C(X-1,
-   * X-1+Y-1) paths (e.g., X-1+Y-1 choose X-1): (X-1 + Y-1)! / ((X-1)! *
-   * (Y-1)!) Part 2: Code We can implement a simple recursive algorithm
-   * with backtracking:
+   * For clarity, we will solve this part assuming an X by Y grid. Each
+   * path has (X-1)+(Y-1) steps. Imagine the following paths: X X Y Y X
+   * (move right -> right -> down -> down -> right) X Y X Y X (move
+   * right -> down -> right -> down -> right) ... Each path can be fully
+   * represented by the moves at which we move right. That is, if I were
+   * to ask you which path you took, you could simply say “I moved right
+   * on step 3 and 4.” Since you must always move right X-1 times, and
+   * you have X-1 + Y-1 total steps, you have to pick X-1 times to move
+   * right out of X-1+Y-1 choices. Thus, there are C(X-1, X-1+Y-1) paths
+   * (e.g., X-1+Y-1 choose X-1): (X-1 + Y-1)! / ((X-1)! * (Y-1)!) We can
+   * implement a simple recursive algorithm with backtracking:
    */
 
   public static Collection<List<Point>> getPaths(final int boardWidth,
