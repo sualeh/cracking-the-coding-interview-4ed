29db5bf9c250709c46bcc06a9b8939de4a7854ec
Sualeh Fatehi <sualeh@hotmail.com>
2014-02-04 19:34:28 -0500

    8.1

---
+++ src/test/java/us/fatehi/sualeh/chapter08/TestQuestion08_1.java
@@ -0,0 +1,42 @@
+package us.fatehi.sualeh.chapter08;
+
+
+import static org.junit.Assert.assertEquals;
+import static us.fatehi.sualeh.chapter08.Question08_1.fibo_iterative;
+import static us.fatehi.sualeh.chapter08.Question08_1.fibo_recursive;
+
+import org.junit.Test;
+
+import us.fatehi.sualeh.testutil.TreeNodeTest;
+
+public class TestQuestion08_1
+  extends TreeNodeTest
+{
+
+  @Test
+  public void test1Question08_1()
+  {
+
+    assertEquals(-1, fibo_iterative(-1));
+    assertEquals(0, fibo_iterative(0));
+    assertEquals(1, fibo_iterative(1));
+    assertEquals(1, fibo_iterative(2));
+    assertEquals(2, fibo_iterative(3));
+    assertEquals(34, fibo_iterative(9));
+
+  }
+
+  @Test
+  public void test1Question08_2()
+  {
+
+    assertEquals(-1, fibo_recursive(-1));
+    assertEquals(0, fibo_recursive(0));
+    assertEquals(1, fibo_recursive(1));
+    assertEquals(1, fibo_recursive(2));
+    assertEquals(2, fibo_recursive(3));
+    assertEquals(34, fibo_recursive(9));
+
+  }
+
+}
